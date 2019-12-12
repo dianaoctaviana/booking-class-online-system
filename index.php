747270ff799efaedca22aa10bc05edc19a13cfef
dianaoctaviana <58798944+dianaoctaviana@users.noreply.github.com>
2019-12-12 10:37:56 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+        <html>
+        <head>
+            <script src='jquery/jquery-2.1.4.min.js' ></script>
+            <script src='jquery/js.js'> </script>
+            <link rel ='stylesheet' href='css/css.css'> 
+            <title>Booking Class Online System </title>
+</head>
+<body>
+	<img src="images/kelas.jpg" style="width:100%; height:100%;">
+<center>
+	<form action='login.php?op=in' method='POST'>
+<table>                 	
+<div class='box-center'>
+<div id='form'>
+ <div class='logo'>
+ <svg version='1.1' id='Layer_1' xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink' x='0px' y='0px' viewBox='0 0 458.8 167.5' enable-background='new 0 0 458.8 167.5' xml:space='preserve'>
+ <div class='box-center'><img src ='images/logo.png' width='100px'> <h3 style="color: #ffffff;"> Booking Class Online System </h3>
+ </div> 
+ </svg>
+ </div>
+     <div class='form-item'>
+ <p class='formLabel'>Username</p>
+  <input type='text' name='user' class='form-style' autocomplete='off'/>
+ </div>
+<div class='form-item'>
+ <p class='formLabel'>Password</p>
+ <input type='password' name='password' class='form-style'/>
+  </div>
+  <div class='form-item'>
+<input type='submit' class='login pull-right' value='LOGIN'>
+<div class='clear-fix'></div>
+</div>
+</div>
+</div>
+</table>
+</form>
+</center>
+</body>
+
